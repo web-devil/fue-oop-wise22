c0987078dddf906d2cb5768b810a504f657bfa85
Andreas Paech <me@andreas-paech.de>
2021-11-18 19:03:53 +0100

    .

--- TicTacToeSpielfeld/Spielfeld.java
+++ TicTacToeSpielfeld/Spielfeld.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 
 public class Spielfeld {
-    char[] fields = new char[9];
-    char nextPlayer = 'x';
-    Scanner input;
+    private char[] fields = new char[9];
+    private char currentPlayer = 'x';
+    private Scanner input;
 
     // Variablennamen dienen NUR der Lesbarkeit!
     // (Sind aber deswegen umso wichtiger)
@@ -19,23 +19,25 @@ public class Spielfeld {
         for(int i = 1; i <= 9; i++) {
             System.out.println("Welches Feld soll's denn sein?");
             int selectedField = this.input.nextInt() - 1;
-            char currentPlayer = this.nextPlayer;
             
-            if (!this.setField(selectedField, currentPlayer)) {
+            // nested if/else vermeiden! lieber mehrere kurze ifs hintereinander
+            if (!this.setField(selectedField)) {
                 i--;
+                continue;
             }
             
-            if (this.hasWon(currentPlayer)) {
-                System.out.println("Spieler " + currentPlayer + " hat gewonnen!");
+            this.print();
+
+            if (this.hasWon()) {
+                System.out.println("Spieler " + this.currentPlayer + " hat gewonnen!");
                 break; // continue NICHT, aber return, i=10
             }
-            
+
             this.setNextPlayer();
-            this.print();
         }
     }
 
-    public boolean setField(int selectedField, char currentPlayer) {
+    public boolean setField(int selectedField) {
         if (this.fields[selectedField] != ' ') {
             System.out.println();
             System.out.println("Das Feld ist belegt!");
@@ -43,18 +45,18 @@ public class Spielfeld {
             return false; // Alternative: else
         }
 
-        this.fields[selectedField] = currentPlayer;
+        this.fields[selectedField] = this.currentPlayer;
         return true;
     }
 
     private void setNextPlayer() {
         // TERNARY OPERATOR = Kurzschreibweise
         // "Bedingte Zuweisung" (im Kurstext)
-        this.nextPlayer = (this.nextPlayer == 'o') ? 'x' : 'o';
-        /*if (this.nextPlayer == 'o') {
-            this.nextPlayer = 'x';
+        this.currentPlayer = (this.currentPlayer == 'o') ? 'x' : 'o';
+        /*if (this.currentPlayer == 'o') {
+            this.currentPlayer = 'x';
         } else {
-            this.nextPlayer = 'o';
+            this.currentPlayer = 'o';
         }*/
     }
 
@@ -67,7 +69,9 @@ public class Spielfeld {
         System.out.println();
     }
 
-    private boolean hasWon(String playerChar) {
+    private boolean hasWon() { // char --> string TYPE CASTING
+        char playerChar = this.currentPlayer;
+
         return (this.fields[0] == playerChar && this.fields[1] == playerChar && this.fields[2] == playerChar)
             || (this.fields[3] == playerChar && this.fields[4] == playerChar && this.fields[5] == playerChar)
             || (this.fields[6] == playerChar && this.fields[7] == playerChar && this.fields[8] == playerChar)
